0a7b5ae266f6c53b06e1937ed1f1b995de59f60f
mgarcia.devinuesa <mgarcia.devinuesa@atsistemas.com>
2023-03-02 15:45:10 +0100

    First step, endpoint with default configuration

---
+++ spring-security-ms/src/main/java/com/example/demo/SpringSecurityMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMsApplication.class, args);
+	}
+
+}
