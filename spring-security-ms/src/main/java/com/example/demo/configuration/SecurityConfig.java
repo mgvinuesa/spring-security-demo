ca93961c23f9297057890fc9b68798ba1d2af9e6
mgarcia.devinuesa <mgarcia.devinuesa@atsistemas.com>
2023-03-02 16:34:52 +0100

    Add specific Security Context Configuration

--- spring-security-ms/src/main/java/com/example/demo/configuration/SecurityConfig.java
+++ spring-security-ms/src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -2,15 +2,38 @@ package com.example.demo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+/*
+ * With Spring Security on the classpath, Spring Boot Security
+ * Auto-Configuration‘s WebSecurityEnablerConfiguration
+ * activates @EnableWebSecurity for us.
+ */
 public class SecurityConfig {
+
+	/*
+	 * https://spring.io/blog/2022/02/21/spring-security-without-the-
+	 * websecurityconfigureradapter
+	 */
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic();
+		return http.build();
+	}
+
+	@Bean
+	public WebSecurityCustomizer ignoreResources() {
+		return (webSecurity) -> webSecurity.ignoring().requestMatchers("/public");
+	}
 
 	@Bean
 	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
